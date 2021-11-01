471225d039e89c799d82879152d4d9ed87519e84
Yousef Obeidat <y.linux96@gmail.com>
2021-11-01 15:48:20 +0200

    fixed

---
+++ math_series_ss/series.py
@@ -0,0 +1,20 @@
+
+def fibonacci(n):
+    
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n > 1:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+
+def lucas(n):
+
+    if n == 0:
+        return 2
+    if n == 1:
+        return 3
+    if n > 1:
+        return lucas(n-1)+lucas(n-2)
+
